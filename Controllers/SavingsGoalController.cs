f011d665c23ca48ee840a4b245f5f6570d7fc33b
Dan H <spag1024@hotmail.com>
2019-07-17 13:45:24 -0500

    working on goal controller

---
+++ Controllers/SavingsGoalController.cs
@@ -0,0 +1,14 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using FinGoals.Models;
+
+namespace FinGoals.Controllers
+{
+    public class SavingsGoalController : ControllerBase 
+    {
+        
+    }
+}
--- Models/SavingsGoal.cs
+++ Models/SavingsGoal.cs
@@ -3,6 +3,7 @@ namespace FinGoals.Models
     public class SavingsGoal
     {
         public long Id { get; set; }
+        public string UserId {get; set; }
         public string Name { get; set; }
         public double Amount { get; set; }
         public bool IsComplete { get; set; }
