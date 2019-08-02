6d5ff38028b8fea19fac3d0acaab342c14b1329e
Dan H <spag1024@hotmail.com>
2019-08-01 20:33:19 -0500

    added model and started DbContext

---
+++ Models/SavingGoal.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FinGoals_DotNet.Models
+{
+    public class SavingGoal
+    {
+        public long Id { get; set; }
+        public string UserId { get; set; }
+        public string Name { get; set; }
+        public double Amount { get; set; }
+        public string Description { get; set; }
+        public bool IsComplete { get; set; }
+    }
+}
